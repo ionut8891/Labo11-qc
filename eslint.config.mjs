fe29c408f8a5a19f477b01d18b44bb9571d2c811
ionut8891 <148498333+ionut8891@users.noreply.github.com>
2024-07-09 08:45:32 -0400

    pipeline

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,4 +10,17 @@ export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...fixupConfigRules(pluginReactConfig),
+  {
+    "rules": {
+      "complexity": ["error", 10],
+      "max-depth": ["error", 4],
+      "max-lines": ["error", 300],
+      "max-lines-per-function": ["error", 50],
+      "max-nested-callbacks": ["error", 3],
+      "max-params": ["error", 4],
+      "max-statements": ["error", 10],
+      "no-var": "error",
+      "eqeqeq": ["error", "always"]
+    }
+  }
 ];
